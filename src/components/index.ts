76410d5414a22d93314d69cfc1eb1e5d53f012de
wangmq <it-wangmq@derucci.net>
2022-01-01 15:58:21 +0800

    项目搭建

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CreateProtal } from './CreateProtal';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as LazyLoad } from './LazyLoad';
+export { default as Modal } from './Modal';
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function () {
+  const [state, setState] = useState();
+  useEffect(() => {}, []);
+
+  return(
+    <div>
+      home
+    </div>
+  )
+}
